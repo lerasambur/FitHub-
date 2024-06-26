cc9307d9883ed396a8c2a8939fac0c9fbeacfa91
NicoleBarkan <eb23122@edu.lu.lv>
2024-06-26 17:48:30 +0300

    Update home.blade.php

--- home.blade.php
+++ home.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container main">
     <h2>Welcome to FitHub</h2>
     <p>Train, share, and achieve your goals with us.</p>
 </div>
 @endsection
 
+@section('styles')
+<style>
+    .container.main {
+        margin: auto;
+        width: 80%;
+        text-align: center;
+        padding: 50px;
+    }
+    h2, p {
+        color: #d4af37;
+    }
+</style>
+@endsection
