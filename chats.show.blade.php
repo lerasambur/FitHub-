fe5873e0ae2d4858e368d7b4ec21631dfd3413c8
NicoleBarkan <eb23122@edu.lu.lv>
2024-06-26 16:29:30 +0300

    Update chats.show.blade.php

--- chats.show.blade.php
+++ chats.show.blade.php
@@ -2,22 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+    <h2>{{ $chat->title }}</h2>
+    <div class="messages">
+        @foreach($chat->messages as $message)
+            <p><strong>{{ $message->user->name }}:</strong> {{ $message->body }}</p>
+        @endforeach
     </div>
+    <form action="{{ route('chats.messages.store', $chat->id) }}" method="POST">
+        @csrf
+        <textarea name="body" required></textarea>
+        <button type="submit">Send</button>
+    </form>
 </div>
 @endsection
