f37cc71d1fef86bc39495de3224e708f8421544a
NicoleBarkan <eb23122@edu.lu.lv>
2024-06-26 18:04:09 +0300

    Update posts.index.blade.php

--- posts.index.blade.php
+++ posts.index.blade.php
@@ -2,14 +2,23 @@
 
 @section('content')
 <div class="container main">
-    <h2>Latest Posts</h2>
-    <div class="posts">
-        @foreach($posts as $post)
-            <div class="post">
-                <h3>{{ $post->title }}</h3>
-                <p>{{ $post->body }}</p>
-                <p>Author: {{ $post->user->name }}</p>
+    <div class="posts-header">
+        <h2>Posts</h2>
+    </div>
+    <div class="posts-content">
+        @foreach ($posts as $post)
+        <div class="post">
+            <div class="post-header">
+                <p><strong>{{ $post->user->name }}</strong> <span>{{ $post->created_at->diffForHumans() }}</span></p>
             </div>
+            <div class="post-body">
+                <p>{{ $post->body }}</p>
+            </div>
+            <div class="post-footer">
+                <p><strong>Comments:</strong> {{ $post->comments_count }}</p>
+                <a href="#" class="button">Comment</a>
+            </div>
+        </div>
         @endforeach
     </div>
 </div>
@@ -20,28 +29,44 @@
     .container.main {
         margin: auto;
         width: 80%;
-        text-align: center;
         padding: 50px;
     }
-    .posts {
-        margin-top: 20px;
+    .posts-header {
+        background-color: #3a3a3a;
+        padding: 20px;
+        text-align: center;
+    }
+    .posts-content {
         background-color: #2c2c2c;
-        padding: 10px;
+        padding: 20px;
         border-radius: 5px;
         color: #d4af37;
-        text-align: left;
+        margin-top: 20px;
     }
     .post {
         margin-bottom: 20px;
-        padding: 10px;
         background-color: #3a3a3a;
+        padding: 15px;
         border-radius: 5px;
     }
-    .post h3 {
+    .post-header {
+        margin-bottom: 10px;
         color: #d4af37;
     }
-    .post p {
+    .post-body {
         color: #fff;
+        margin-bottom: 10px;
+    }
+    .post-footer {
+        color: #d4af37;
+    }
+    .button {
+        background-color: #d4af37;
+        color: #1c1c1c;
+        padding: 5px 10px;
+        text-decoration: none;
+        border-radius: 5px;
+        margin: 5px;
     }
 </style>
 @endsection
