55c1b2d6eaa33fa3dd0fce02bf12df5e3a6b3f91
NicoleBarkan <eb23122@edu.lu.lv>
2024-06-26 16:29:08 +0300

    Update chats.index.blade.php

--- chats.index.blade.php
+++ chats.index.blade.php
@@ -2,22 +2,13 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <h2>Chats</h2>
+    <ul>
+        @foreach($chats as $chat)
+            <li>
+                <a href="{{ route('chats.show', $chat->id) }}">{{ $chat->title }}</a>
+            </li>
+        @endforeach
+    </ul>
 </div>
 @endsection
