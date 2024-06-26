c4919683e682f18b4615f8f3005ebcda8c9258c6
NicoleBarkan <eb23122@edu.lu.lv>
2024-06-26 16:30:20 +0300

    Update posts.create.blade.php

--- posts.create.blade.php
+++ posts.create.blade.php
@@ -2,22 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <h2>Create a Post</h2>
+    <form action="{{ route('posts.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" name="title" class="form-control" required>
         </div>
-    </div>
+        <div class="form-group">
+            <label for="body">Body</label>
+            <textarea name="body" class="form-control" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 </div>
 @endsection
+
