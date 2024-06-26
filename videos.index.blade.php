c665cce0c03e8f86e2abf46afdcfa99df4465b69
NicoleBarkan <eb23122@edu.lu.lv>
2024-06-26 16:31:55 +0300

    Update videos.index.blade.php

--- videos.index.blade.php
+++ videos.index.blade.php
@@ -2,22 +2,14 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <h2>Videos</h2>
+    <ul>
+        @foreach($videos as $video)
+            <li>
+                <a href="{{ route('videos.show', $video->id) }}">{{ $video->title }}</a>
+            </li>
+        @endforeach
+    </ul>
 </div>
 @endsection
+
