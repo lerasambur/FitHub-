9e50db3aab2d4b176c0595eaa6a3efc277d710c8
NicoleBarkan <eb23122@edu.lu.lv>
2024-06-26 16:30:37 +0300

    Update posts.index.blade.php

--- posts.index.blade.php
+++ posts.index.blade.php
@@ -2,22 +2,14 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <h2>Posts</h2>
+    <ul>
+        @foreach($posts as $post)
+            <li>
+                <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
+            </li>
+        @endforeach
+    </ul>
 </div>
 @endsection
+
