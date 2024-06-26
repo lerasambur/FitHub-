da1e8996426d673ed66a03af314d8d4ac7e68da7
NicoleBarkan <eb23122@edu.lu.lv>
2024-06-26 17:52:44 +0300

    Update posts.index.blade.php

--- posts.index.blade.php
+++ posts.index.blade.php
@@ -1,15 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2>Posts</h2>
-    <ul>
+<div class="container main">
+    <h2>Latest Posts</h2>
+    <div class="posts">
         @foreach($posts as $post)
-            <li>
-                <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a>
-            </li>
+            <div class="post">
+                <h3>{{ $post->title }}</h3>
+                <p>{{ $post->body }}</p>
+                <p>Author: {{ $post->user->name }}</p>
+            </div>
         @endforeach
-    </ul>
+    </div>
 </div>
 @endsection
 
+@section('styles')
+<style>
+    .container.main {
+        margin: auto;
+        width: 80%;
+        text-align: center;
+        padding: 50px;
+    }
+    .posts {
+        margin-top: 20px;
+        background-color: #2c2c2c;
+        padding: 10px;
+        border-radius: 5px;
+        color: #d4af37;
+        text-align: left;
+    }
+    .post {
+        margin-bottom: 20px;
+        padding: 10px;
+        background-color: #3a3a3a;
+        border-radius: 5px;
+    }
+    .post h3 {
+        color: #d4af37;
+    }
+    .post p {
+        color: #fff;
+    }
+</style>
+@endsection
