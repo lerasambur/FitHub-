befd2ddb92e9ebd7e29a4fa45fee8548007536c9
NicoleBarkan <eb23122@edu.lu.lv>
2024-06-27 13:59:39 +0300

    Create messages.php

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'welcome' => 'Welcome to FitHub!',
+    'description' => 'Train, share, and achieve your goals with us.',
+    'register' => 'Register',
+    'login' => 'Login',
+    'header' => 'FitHub',
+    'footer' => 'All rights reserved.',
+];
+?>
